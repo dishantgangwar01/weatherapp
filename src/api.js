d1c4751810401e203114ad609b8facc7947e73f9
Dishant Gangwar <94188265+dishantgangwar01@users.noreply.github.com>
2023-07-22 21:18:06 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'f0f6e40d7dmsh117dc814675cb2bp19bd49jsn5159ca3a5f12',
+		'X-RapidAPI-Key': 'YOUR SECERT KEY  ',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "68b377de87acb37e222e273b5fb48c54"
+export const WEATHER_API_KEY = "YOUR API KEY"
